8aba42440efec1c85b7f73f33e4424b2ee7fab16
Amrit Poudel <amritpoudel433@gmail.com>
2025-09-14 16:22:25 +0545

    chore(ci/cd): codebuild permission error

--- ci-cd/main.tf
+++ ci-cd/main.tf
@@ -77,7 +77,8 @@ resource "aws_iam_policy" "codebuild_iam_policy" {
           "s3:GetBucketAcl",
           "s3:GetBucketVersioning",
           "s3:GetBucketCors",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:GetBucketWebsite"
         ],
         Resource = [
           var.backend_bucket_arn,
