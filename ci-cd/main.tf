67d856d0c76ce06a275bf3a86ed1722e73908114
Amrit Poudel <amritpoudel433@gmail.com>
2025-09-14 19:29:57 +0545

    chore(cloudwatch): addded the cloudwatch permission

--- ci-cd/main.tf
+++ ci-cd/main.tf
@@ -144,12 +144,15 @@ resource "aws_iam_policy" "codebuild_iam_policy" {
     "sns:Publish",
     "sns:ListTopics" ,
       "sns:GetTopicAttributes",
+      "sns:GetSubscriptionAttributes",
+      "cloudwatch:ListTagsForResource",
         "sns:ListTagsForResource",
            "cloudwatch:PutMetricAlarm",
     "cloudwatch:DescribeAlarms",
     "cloudwatch:DeleteAlarms",
     "cloudwatch:PutMetricData",
-    "cloudwatch:GetMetricData"
+    "cloudwatch:GetMetricData",
+  
   ]
   Resource = "*"
 }
