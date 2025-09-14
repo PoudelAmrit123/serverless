0122fc1ad8e1267f2f1e7a66d9359761d1eb81bd
Amrit Poudel <amritpoudel433@gmail.com>
2025-09-14 17:25:20 +0545

    chore(ci/cd): fixed python error

--- ci-cd/main.tf
+++ ci-cd/main.tf
@@ -157,7 +157,7 @@ resource "aws_codebuild_project" "codebuild_project" {
   }
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/standard:7.0"
     type                        = "LINUX_CONTAINER"
     privileged_mode             = false
   }
